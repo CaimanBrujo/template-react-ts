45be05423826641b58f7e9910f927a247cbebd0b
Caimanbrujo <186770762+CaimanBrujo@users.noreply.github.com>
2025-06-27 17:56:43 +0200

    feat: update footer with Github logo and personal link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,20 @@
+import { Github } from 'lucide-react'
+
 export default function Footer() {
   return (
-    <footer className="w-full bg-background text-muted py-4 px-6 border-t border-[--color-border] mt-auto">
-      <p className="text-sm text-center">
-        © {new Date().getFullYear()} Template | Caiman Brujo | All rights
-        reserved.
+    <footer className="w-full bg-background text-muted py-6 px-6 border-t border-border mt-auto">
+      <p className="text-sm text-center flex items-center justify-center gap-2">
+        © {new Date().getFullYear()} Template |
+        <a
+          href="https://github.com/CaimanBrujo"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="inline-flex items-center gap-1 hover:text-title text-muted"
+        >
+          <Github className="w-4 h-4" />
+          Caiman Brujo
+        </a>{' '}
+        | All rights reserved.
       </p>
     </footer>
   )
