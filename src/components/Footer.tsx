f6c28ca39f56ef0c2c6dee417a7f568c8ec1e710
Caimanbrujo <186770762+CaimanBrujo@users.noreply.github.com>
2025-06-27 15:40:03 +0200

    feat(theme): create ThemeToggle component and integrate it into header layout

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="w-full bg-background text-muted py-4 px-6 border-t border-border mt-auto">
+    <footer className="w-full bg-background text-muted py-4 px-6 border-t border-[--color-border] mt-auto">
       <p className="text-sm text-center">
         © {new Date().getFullYear()} Template | Caiman Brujo | All rights
         reserved.
